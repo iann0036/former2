c56574287bd5ec6916cf6e8f1c5f24f6f027e809
Ian Mckay <contact@ianmckay.com.au>
2019-12-21 23:08:32 +1100

    CLI update

--- cli/README.md
+++ cli/README.md
@@ -1,11 +1,23 @@
-> The Former2 CLI is experimental. Use at your own risk.
+# Former2 CLI
+
+The Former2 CLI allows you to use the [former2.com](https://former2.com) tool directly from your command line.
+
+:exclamation: **CAUTION:** The Former2 CLI is experimmental. Use at your own risk.
 
 ## Install
 
-...
+npm install former2
 
 ## Usage
 
+### generate
+
+The `generate` command will generate one or multiple outputs from all discovered resources and write them to the filename specified.
+
 ```
-node main.js --output-cloudformation "cloudformation.yml" --output-terraform "terraform.hcl" --output-debug "debug.json"
+former2 generate --output-cloudformation "cloudformation.yml" --output-terraform "terraform.hcl" --output-debug "debug.json"
 ```
+
+## Security
+
+Calls to the AWS service API endpoints are made directly with the JavaScript SDK. Recording data is kept entirely in memory or on local disk and is never sent over the internet or anywhere else. You should take care to remove any sensitive data (passwords etc.) when sharing your generated code/templates with others.
--- cli/main.js
+++ cli/main.js
@@ -1,10 +1,12 @@
 var AWS = require("aws-sdk");
 const fs = require('fs');
+const process = require('process');
 const deepmerge = require('deepmerge');
 const cliargs = require('commander');
 const cliprogress = require('cli-progress');
 const logplease = require('logplease');
 const _colors = require('colors');
+const pjson = require('./package.json');
 
 logplease.setLogLevel('NONE');
 const awslog = logplease.create('AWS');
@@ -42,7 +44,12 @@ f2trace = function(err){};
 
 var completed_sections = [];
 
-async function main(cliargs) {
+async function main(opts) {
+    if (!opts.outputDebug && !opts.outputCloudformation && !opts.outputTerraform) {
+        console.warn("You must specify an output type");
+        return;
+    }
+
     sections.forEach(section => {
         let dtname = 'updateDatatable' + nav(section.category) + nav(section.service);
         eval("(async () => { try { await " + dtname + "(); } finally { completed_sections.push('" + section + "'); } })()");
@@ -52,11 +59,10 @@ async function main(cliargs) {
         format: _colors.cyan('{bar}') + '  {percentage}% ({value}/{total} services completed)',
         barCompleteChar: '\u2588',
         barIncompleteChar: '\u2591',
-        hideCursor: true
+        hideCursor: false
     });
     
     b1.start(sections.length, 0);
-    b1.update(20);
 
     while (completed_sections.length < sections.length) {
         await sleep(1000);
@@ -65,13 +71,13 @@ async function main(cliargs) {
 
     b1.stop();
 
-    if (cliargs.outputDebug) {
-        fs.writeFile(cliargs.outputDebug, JSON.stringify(cli_resources, null, 4), (err) => {
+    if (opts.outputDebug) {
+        fs.writeFile(opts.outputDebug, JSON.stringify(cli_resources, null, 4), (err) => {
             if (err) throw err;
         });
     }
 
-    if (cliargs.outputCloudformation || cliargs.outputTerraform) {
+    if (opts.outputCloudformation || opts.outputTerraform) {
         var output_objects = [];
 
         for (var i=0; i<cli_resources.length; i++) {
@@ -86,25 +92,34 @@ async function main(cliargs) {
         var tracked_resources = performF2Mappings(output_objects);
         var mapped_outputs = compileOutputs(tracked_resources, null);
 
-        if (cliargs.outputCloudformation) {
-            fs.writeFile(cliargs.outputCloudformation, mapped_outputs['cfn'], (err) => {
+        if (opts.outputCloudformation) {
+            fs.writeFile(opts.outputCloudformation, mapped_outputs['cfn'], (err) => {
                 if (err) throw err;
             });
         }
 
-        if (cliargs.outputTerraform) {
-            fs.writeFile(cliargs.outputTerraform, mapped_outputs['tf'], (err) => {
+        if (opts.outputTerraform) {
+            fs.writeFile(opts.outputTerraform, mapped_outputs['tf'], (err) => {
                 if (err) throw err;
             });
         }
     }
 }
 
+var validaction = false;
 cliargs
+    .version(pjson.version)
+    .command('generate')
+    .description('generates outputs and writes them to the specified file')
     .option('--output-cloudformation <filename>', 'filename for CloudFormation output')
     .option('--output-terraform <filename>', 'filename for Terraform output')
-    .option('--output-debug <filename>', 'filename for debug output (full)');
+    .option('--output-debug <filename>', 'filename for debug output (full)')
+    .action(opts => {
+        validaction = true;
+        main(opts);
+    });
 
 cliargs.parse(process.argv);
-
-main(cliargs);
+if (!validaction) {
+    cliargs.help();
+}
