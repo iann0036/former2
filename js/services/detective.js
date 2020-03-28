4d4aa79b93dd35d999f049ef8f33ac05e62a0f85
Ian Mckay <contact@ianmckay.com.au>
2020-03-29 01:00:11 +1100

    Add Detective resources

---
+++ js/services/detective.js
@@ -0,0 +1,167 @@
+/* ========================================================================== */
+// Detective
+/* ========================================================================== */
+
+sections.push({
+    'category': 'Security, Identity &amp; Compliance',
+    'service': 'Detective',
+    'resourcetypes': {
+        'Graph': {
+            'columns': [
+                [
+                    {
+                        field: 'state',
+                        checkbox: true,
+                        rowspan: 2,
+                        align: 'center',
+                        valign: 'middle'
+                    },
+                    {
+                        title: 'ARN',
+                        field: 'arn',
+                        rowspan: 2,
+                        align: 'center',
+                        valign: 'middle',
+                        sortable: true,
+                        formatter: primaryFieldFormatter,
+                        footerFormatter: textFormatter
+                    }
+                ],
+                [
+                    // nothing
+                ]
+            ]
+        },
+        'Member Invitations': {
+            'columns': [
+                [
+                    {
+                        field: 'state',
+                        checkbox: true,
+                        rowspan: 2,
+                        align: 'center',
+                        valign: 'middle'
+                    },
+                    {
+                        title: 'Member ID',
+                        field: 'memberid',
+                        rowspan: 2,
+                        align: 'center',
+                        valign: 'middle',
+                        sortable: true,
+                        formatter: primaryFieldFormatter,
+                        footerFormatter: textFormatter
+                    },
+                    {
+                        title: 'Properties',
+                        colspan: 4,
+                        align: 'center'
+                    }
+                ],
+                [
+                    {
+                        field: 'grapharn',
+                        title: 'Graph ARN',
+                        sortable: true,
+                        editable: true,
+                        footerFormatter: textFormatter,
+                        align: 'center'
+                    },
+                    {
+                        field: 'memberemailaddress',
+                        title: 'Member Email Address',
+                        sortable: true,
+                        editable: true,
+                        footerFormatter: textFormatter,
+                        align: 'center'
+                    }
+                ]
+            ]
+        }
+    }
+});
+
+async function updateDatatableSecurityIdentityAndComplianceDetective() {
+    blockUI('#section-securityidentityandcompliance-detective-graph-datatable');
+    blockUI('#section-securityidentityandcompliance-detective-memberinvitations-datatable');
+
+    await sdkcall("Detective", "listGraphs", {
+        // no params
+    }, false).then(async (data) => {
+        $('#section-securityidentityandcompliance-detective-graph-datatable').bootstrapTable('removeAll');
+
+        if (data.GraphList && data.GraphList.length) {
+            $('#section-securityidentityandcompliance-detective-graph-datatable').deferredBootstrapTable('append', [{
+                f2id: data.GraphList[0].Arn,
+                f2type: 'detective.graph',
+                f2data: {
+                    'Arn': data.GraphList[0].Arn
+                },
+                f2region: region,
+                arn: data.GraphList[0].Arn
+            }]);
+        }
+    }).catch(() => { });
+
+    await sdkcall("Detective", "listInvitations", {
+        // no params
+    }, false).then(async (data) => {
+        console.log(data);
+        $('#section-securityidentityandcompliance-detective-memberinvitations-datatable').bootstrapTable('removeAll');
+
+        data.Invitations.forEach(invitation => {
+            if (invitation.EmailAddress) { // master is null
+                $('#section-securityidentityandcompliance-detective-memberinvitations-datatable').deferredBootstrapTable('append', [{
+                    f2id: invitation.GraphArn + " invitation to " + invitation.MasterId,
+                    f2type: 'detective.memberinvitation',
+                    f2data: invitation,
+                    f2region: region,
+                    grapharn: invitation.GraphArn,
+                    memberid: invitation.AccountId,
+                    memberemailaddress: invitation.EmailAddress
+                }]);
+            }
+        });
+    }).catch(() => { });
+
+    unblockUI('#section-securityidentityandcompliance-detective-graph-datatable');
+    unblockUI('#section-securityidentityandcompliance-detective-memberinvitations-datatable');
+}
+
+service_mapping_functions.push(function(reqParams, obj, tracked_resources){
+    if (obj.type == "detective.graph") {
+        tracked_resources.push({
+            'obj': obj,
+            'logicalId': getResourceName('detective', obj.id, 'AWS::Detective::Graph'),
+            'region': obj.region,
+            'service': 'detective',
+            'type': 'AWS::Detective::Graph',
+            'options': reqParams,
+            'returnValues': {
+                'Ref': obj.data.Arn
+            }
+        });
+    } else if (obj.type == "detective.memberinvitation") {
+        reqParams.cfn['GraphArn'] = obj.data.GraphArn;
+        reqParams.cfn['MemberEmailAddress'] = obj.data.EmailAddress || "REPLACEME";
+        reqParams.cfn['MemberId'] = obj.data.AccountId;
+
+        /*
+        SKIPPED:
+        Message
+        */
+
+        tracked_resources.push({
+            'obj': obj,
+            'logicalId': getResourceName('detective', obj.id, 'AWS::Detective::MemberInvitation'),
+            'region': obj.region,
+            'service': 'detective',
+            'type': 'AWS::Detective::MemberInvitation',
+            'options': reqParams
+        });
+    } else {
+        return false;
+    }
+
+    return true;
+});
